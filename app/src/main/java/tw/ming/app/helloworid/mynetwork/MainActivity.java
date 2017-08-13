5e63fa0c7a7e5219e5517dbafd5ad02292fa84a4
LBJ <LBJ>
2017-08-13 16:31:46 +0800

    20170813

--- app/src/main/java/tw/ming/app/helloworid/mynetwork/MainActivity.java
+++ app/src/main/java/tw/ming/app/helloworid/mynetwork/MainActivity.java
@@ -1,13 +1,82 @@
 package tw.ming.app.helloworid.mynetwork;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView img;
+    private Bitmap bmp;
+    private UIHandler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new UIHandler();
+        img = (ImageView)findViewById(R.id.img);
     }
+    public void test1(View view){
+        //因不能使用於主執行緒,則需增加其他執行緒
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://www.iii.org.tw/");
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                    conn.connect();
+                    BufferedReader br =
+                            new BufferedReader(
+                                    new InputStreamReader(conn.getInputStream()));
+                    String line;
+                    while ( (line = br.readLine()) !=null){
+                        Log.i("ming", line);
+                    }
+                    br.close();
+                } catch (Exception e) {
+                    Log.i("ming", e.toString());
+                }
+
+            }
+        }.start();
+    }
+    public void test2(View view){
+        new Thread(){
+            @Override
+            public void run() {
+                try{
+                    URL url = new URL("http://www.iii.org.tw/assets/images/information-news/image004.jpg");
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                    conn.connect();
+                    bmp = BitmapFactory.decodeStream(conn.getInputStream());
+                    handler.sendEmptyMessage(0);
+                }catch(Exception e){
+                    Log.i("ming", e.toString());
+                }
+            }
+        }.start();
+    }
+
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            img.setImageBitmap(bmp);
+        }
+    }
+
 }
